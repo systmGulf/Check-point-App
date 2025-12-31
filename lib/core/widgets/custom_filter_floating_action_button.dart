f5c01385657485034b51398ba7275e75b6adef8a
kareem_m0hmed <kareemmhammed53@gmail.com>
2025-12-31 12:59:23 +0200

    feat: add custom Filter Button

---
+++ lib/core/widgets/custom_filter_floating_action_button.dart
@@ -0,0 +1,46 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:employee_mangement/core/styles/colors.dart';
+import 'package:employee_mangement/core/styles/styles.dart';
+import 'package:employee_mangement/core/utils/assets_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CustomFilterFloatingActionButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  const CustomFilterFloatingActionButton({super.key, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: ColorsManger.lightblack.withValues(alpha: 0.3),
+              blurRadius: 10,
+              offset: const Offset(-2, 0),
+            ),
+          ],
+          color: Colors.white,
+          border:
+              Border.all(color: ColorsManger.lightblack.withValues(alpha: 0.3)),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          child: GestureDetector(
+            onTap: onPressed,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              spacing: 8,
+              children: [
+                SvgPicture.asset(Assets.assetsImagesFilterButton),
+                Text(
+                  "Filter".tr(context: context),
+                  style: AppStylesManger.font14Medium,
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
