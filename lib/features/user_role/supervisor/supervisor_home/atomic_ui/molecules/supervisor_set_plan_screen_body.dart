c5ceffa7261a4fd3ef15bf0e91ef83282d52eec2
kareem_m0hmed <kareemmhammed53@gmail.com>
2025-12-31 17:12:28 +0200

    feat: add search in plans screen

--- lib/features/user_role/supervisor/supervisor_home/atomic_ui/molecules/supervisor_set_plan_screen_body.dart
+++ lib/features/user_role/supervisor/supervisor_home/atomic_ui/molecules/supervisor_set_plan_screen_body.dart
@@ -1,13 +1,16 @@
 import 'dart:developer';
 
 import 'package:easy_localization/easy_localization.dart';
+import 'package:employee_mangement/core/utils/assets_manager.dart';
 import 'package:employee_mangement/core/widgets/custom_floating_action_button.dart';
 import 'package:employee_mangement/core/widgets/no_interet_connextion_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
 import '../../../../../../core/helpers/app_spaces.dart';
+import '../../../../../../core/widgets/custom_app_text_form_field.dart';
 import '../../../../../../core/widgets/no_data_found_animation_widget.dart';
 import '../../contoller/plan_cubit/plan_cubit.dart';
 import '../atoms/plan_item.dart';
@@ -23,9 +26,15 @@ class SetPlanScreen extends StatelessWidget {
   }
 }
 
-class PlansScreen extends StatelessWidget {
+class PlansScreen extends StatefulWidget {
   const PlansScreen({super.key});
 
+  @override
+  State<PlansScreen> createState() => _PlansScreenState();
+}
+
+class _PlansScreenState extends State<PlansScreen> {
+  String query = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,45 +77,78 @@ class PlansScreen extends StatelessWidget {
                   );
           }
           if (state is GetPlanSuccess) {
+            final filterList = state.planModel.data!
+                .where(
+                  (element) => element.planDate!.toLowerCase().contains(
+                        query.toLowerCase(),
+                      ),
+                )
+                .toList();
+
+            final plan =
+                filterList.isEmpty ? state.planModel.data! : filterList;
             return state.planModel.data!.isNotEmpty
                 ? RefreshIndicator(
                     onRefresh: () async {
                       context.read<PlanCubit>().getPlan();
                     },
-                    child: ListView.builder(
-                        itemCount: state.planModel.data!.length,
-                        itemBuilder: (context, index) {
-                          return Column(
-                            children: [
-                              PlanItem(
-                                planId: state.planModel.data![index].id!,
-                                planDate:
-                                    state.planModel.data![index].planDate ?? '',
-                                note: state.planModel.data![index].note ?? '',
-                                onTap: () {
-                                  context.read<PlanCubit>().planId =
-                                      state.planModel.data![index].id!;
-                                  log(state.planModel.data![index].id!
-                                      .toString());
-                                  Navigator.push(context,
-                                      MaterialPageRoute(builder: (_) {
-                                    return BlocProvider.value(
-                                      value: context.read<PlanCubit>()
-                                        ..getPlanById(
-                                            id: state
-                                                .planModel.data![index].id!),
-                                      child: SubPlansScreen(
-                                        planId:
-                                            state.planModel.data![index].id!,
-                                      ),
-                                    );
-                                  }));
-                                },
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: CustomAppTextFormField(
+                            onChanged: (value) {
+                              setState(() {
+                                query = value;
+                                log("query: $query");
+                              });
+                            },
+                            hint: "Search employee...".tr(context: context),
+                            prefixIcon: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: SvgPicture.asset(
+                                Assets.assetsImagesSearchIcon,
+                                height: 20,
+                                width: 20,
                               ),
-                            ],
-                          );
-                        }),
-                  )
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                              itemCount: plan.length,
+                              itemBuilder: (context, index) {
+                                return Column(
+                                  spacing: 16,
+                                  children: [
+                                    PlanItem(
+                                      planId: plan[index].id!,
+                                      planDate: plan[index].planDate ?? '',
+                                      note: plan[index].note ?? '',
+                                      onTap: () {
+                                        context.read<PlanCubit>().planId =
+                                            plan[index].id!;
+                                        log(state.planModel.data![index].id!
+                                            .toString());
+                                        Navigator.push(context,
+                                            MaterialPageRoute(builder: (_) {
+                                          return BlocProvider.value(
+                                            value: context.read<PlanCubit>()
+                                              ..getPlanById(
+                                                  id: plan[index].id!),
+                                            child: SubPlansScreen(
+                                              planId: plan[index].id!,
+                                            ),
+                                          );
+                                        }));
+                                      },
+                                    ),
+                                  ],
+                                );
+                              }),
+                        ),
+                      ],
+                    ))
                 : NoDataFound();
           }
           return ListView.builder(
