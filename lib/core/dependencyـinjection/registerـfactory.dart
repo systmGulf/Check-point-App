a0f0b4ca1f00d08ca5a77ac04e0701c5c2fd87f7
kareem_m0hmed <kareemmhammed53@gmail.com>
2026-04-18 16:14:52 +0200

    feat: create complaints screen and get employee complaints

--- lib/core/dependencyـinjection/registerـfactory.dart
+++ lib/core/dependencyـinjection/registerـfactory.dart
@@ -3,10 +3,13 @@ import 'package:employee_mangement/features/user_role/admin/admin_home/controlle
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hr_management_system_package/admin_infrastructure/data/repo/shifts_and_polices_repo/shifts_and_polices_repo.dart';
+import 'package:hr_management_system_package/core/dependecy_injection/service_locator.dart'
+    as service_locator;
 import 'package:hr_management_system_package/core/repos/shared_repo.dart';
 import 'package:hr_management_system_package/employee_infrastructure/data/repo/employee_attendance_repo/employee_attendance_repo.dart';
 import 'package:hr_management_system_package/hr_manamgement_system_package.dart';
 import 'package:hr_management_system_package/register_account/repo/register_account_repo.dart';
+import 'package:hr_management_system_package/supervisor_infrastructure/data/repo/complaints/complaints_repo_impl.dart';
 import 'package:hr_management_system_package/supervisor_infrastructure/data/repo/supervisor_attendance_repo/supervisor_attendance_repo.dart';
 import 'package:hr_management_system_package/supervisor_infrastructure/data/repo/supervisor_leave_requests_repo/supervisor_leave_requests_repo.dart';
 import 'package:hr_management_system_package/supervisor_infrastructure/data/repo/supervisor_news_repo/supervisor_news_repo.dart';
@@ -20,6 +23,7 @@ import '../../features/user_role/admin/admin_home/controllers/department_cubit/d
 import '../../features/user_role/admin/admin_home/controllers/mange_employee_cubit/employee_cubit.dart';
 import '../../features/user_role/employee/employee_home/controller/attendence/attendence_cubit.dart';
 import '../../features/user_role/employee/employee_home/controller/change_password/change_password_cubit.dart';
+import '../../features/user_role/employee/employee_home/controller/complaints/complaints_cubit.dart';
 import '../../features/user_role/employee/employee_home/controller/get_employee_history/get_employee_history_cubit.dart';
 import '../../features/user_role/employee/employee_home/controller/leave_application/leave_application_cubit.dart';
 import '../../features/user_role/employee/employee_home/controller/tasks/tasks_cubit.dart';
@@ -71,6 +75,16 @@ void registerFactory() {
   getIt.registerFactory<EmployeeTasksCubit>(
     () => EmployeeTasksCubit(
       getIt<EmployeeActionRepo>(),
+    ),
+  );
+  getIt.registerLazySingleton<ComplaintsRepo>(
+    () => ComplaintsRepoImpl(
+      apiService: service_locator.getIt<ApiService>(),
+    ),
+  );
+  getIt.registerFactory<ComplaintsCubit>(
+    () => ComplaintsCubit(
+      getIt<ComplaintsRepo>(),
     ),
   );
   getIt.registerFactory<EmployeeCubit>(
