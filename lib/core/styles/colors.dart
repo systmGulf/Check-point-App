aad531f746efb9fcf2bacee4b9d2370af0d4ab3a
osamamahmouddev <osama.mahmoud.moahmaed@gmail.com>
2026-07-27 18:50:07 +0300

    feat: change app brand logo, assets, and update colors to gold theme

--- lib/core/styles/colors.dart
+++ lib/core/styles/colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ColorsManger {
-  static Color primaryColor = const Color.fromARGB(255, 76, 98, 192);
-  static const lightGreen = Color(0XFFf5f9ef);
+  static Color primaryColor = const Color(0xFFB1871E);
+  static const lightGreen = Color(0xFFFCF8EE);
   static Color lighorage = const Color(0xFFcfcdc9);
-  static Color darkGreen = const Color.fromARGB(255, 71, 110, 66);
+  static Color darkGreen = const Color(0xFF7C5F15);
   static Color grey = const Color(0XFFbfbfbf);
   static Color lightblack = const Color(0XFF414042);
   static Color darkblue = const Color(0XFF000000);
