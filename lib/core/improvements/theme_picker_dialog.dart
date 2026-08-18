903d56813fda2996850bcc6d476feed93b66e129
osamamahmouddev <osama.mahmoud.moahmaed@gmail.com>
2026-08-18 14:01:55 +0300

    feat(localization): add translations for Change Theme Color option and Dialog title

--- lib/core/improvements/theme_picker_dialog.dart
+++ lib/core/improvements/theme_picker_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'dynamic_theme_cubit.dart';
 
 class ThemePickerDialog extends StatelessWidget {
@@ -16,7 +17,7 @@ class ThemePickerDialog extends StatelessWidget {
     ];
 
     return AlertDialog(
-      title: const Text('Select Theme Color'),
+      title: Text('Select Theme Color'.tr()),
       content: SizedBox(
         width: double.maxFinite,
         child: GridView.builder(
