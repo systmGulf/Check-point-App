dbef6d547aeea8e6870fc4d27d64ef83c4949b6e
osamamahmouddev <osama.mahmoud.moahmaed@gmail.com>
2026-08-18 15:53:58 +0300

    feat(theme): change app primaryColor to Brand Royal Blue (0xFF1D50D7) matching brand mockup

--- lib/core/styles/colors.dart
+++ lib/core/styles/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ColorsManger {
   static Color scaffoldBackgroundColor = const Color(0XFFFFFFFF);
-  static Color primaryColor = const Color(0xFF757575); // Classic Grey
+  static Color primaryColor = const Color(0xFF1D50D7); // Brand Royal Blue
   static Color primaryColorLight = const Color(0xFF6B7280); // Cool Grey
   static const lightGreen = Color(0xFFF3F4F6); // Light Grey tint
   static Color lighorage = const Color(0xFF4B5563); // Medium Grey
